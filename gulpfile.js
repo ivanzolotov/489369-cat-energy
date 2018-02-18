3e106a0a1a25ee2924710cc141ac808345d45a84
ivanzolotov <ivanzolotov@ivanzolotov.ru>
2018-02-18 20:44:40 +0500

    Настроена базовая сборка проекта

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@ var sass = require("gulp-sass");
 var plumber = require("gulp-plumber");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
+var cssmin = require('gulp-cssmin');
 var server = require("browser-sync").create();
 
 gulp.task("style", function() {
@@ -16,6 +17,29 @@ gulp.task("style", function() {
     ]))
     .pipe(gulp.dest("source/css"))
     .pipe(server.stream());
+});
+
+gulp.task('buildHtml', function () {
+  gulp.src('./source/*.html')
+    .pipe(gulp.dest('./build'));
+});
+
+gulp.task('buildFonts', function () {
+  gulp.src('./source/fonts/**/*.{woff,woff2}')
+    .pipe(gulp.dest('./build/fonts'));
+});
+
+gulp.task('buildImages', function () {
+  gulp.src('./source/img/**/*.{png,jpg,svg}')
+    .pipe(gulp.dest('./build/img'));
+});
+
+gulp.task('buildStyles', function () {
+  gulp.src('./source/css/style.css')
+    .pipe(gulp.dest('./build/css'));
+});
+
+gulp.task("build", ["buildStyles", "buildHtml", "buildFonts", "buildImages"], function() {
 });
 
 gulp.task("serve", ["style"], function() {
